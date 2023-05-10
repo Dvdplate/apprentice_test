4f5fa30f1379f0e4017d77dad9a72ad40ceb5423
Dvdplate <94102213+Dvdplate@users.noreply.github.com>
2023-05-10 14:02:55 +0200

    Update Client.ts

--- Client.ts
+++ Client.ts
@@ -1,14 +1,15 @@
-control.simmessages.onReceived("apprentice_Car", (msg: Buffer) => {
-    let stringMsg = ""
-    for (let i = 0; i < msg.length; i++) {
-        stringMsg += String.fromCharCode(msg[i]);
-    }
-    let data = JSON.parse(stringMsg)
-    console.log("---->")
-    console.log(data.ultrasonicDistance)
-    console.log(data.leftIRObstacleSensor)
-    console.log(data.rightIRObstacleSensor)
-    console.log(data.leftIRLineSensor)
-    console.log(data.rightIRLineSensor)
-})
+namespace kBit {
+    control.simmessages.onReceived("apprentice_Car", (msg: Buffer) => {
+        let stringMsg = ""
+        for (let i = 0; i < msg.length; i++) {
+            stringMsg += String.fromCharCode(msg[i]);
+        }
+        let data = JSON.parse(stringMsg)
+        assignInputs(data)
+    })
 
+
+    function assignInputs(data: any) {
+        
+    }
+}
