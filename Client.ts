7f97462d80c258d4d30740df93e8b83368e73126
Dvdplate <94102213+Dvdplate@users.noreply.github.com>
2023-05-10 14:31:33 +0200

    Update Client.ts

--- Client.ts
+++ Client.ts
@@ -1,15 +1,21 @@
 namespace kBit {
+    export let ultrasonicSimData: number;
+    export let leftObstacleSimData: boolean;
+    export let rightObstacleSimData: boolean;
+    export let leftLineSimData: boolean;
+    export let rightLineSimData: boolean;
+
     control.simmessages.onReceived("apprentice_Car", (msg: Buffer) => {
         let stringMsg = ""
         for (let i = 0; i < msg.length; i++) {
             stringMsg += String.fromCharCode(msg[i]);
         }
         let data = JSON.parse(stringMsg)
-        assignInputs(data)
+
+        kBit.ultrasonicSimData = data.ultrasonicDistance;
+        kBit.leftObstacleSimData = data.leftIRObstacleSensor;
+        kBit.rightObstacleSimData = data.rightIRObstacleSensor;
+        kBit.leftLineSimData = data.leftIRLineSensor
+        kBit.rightLineSimData = data.rightIRLineSensor
     })
-
-
-    function assignInputs(data: any) {
-        
-    }
 }
